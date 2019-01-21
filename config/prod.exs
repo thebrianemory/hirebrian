28cee49f468a5c726a12a83005cf1972549e970b
thebrianemory <the@brianemory.com>
2019-01-20 22:43:10 -0800

    Set up nanobox

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :hirebrian, HirebrianWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: 8080],
+  load_from_system_env: true,
+  url: [host: "hirebrian.today", port: 80],
+  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
